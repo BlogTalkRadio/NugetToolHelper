928cd489bbb68bfe2a266db8875a74d069d7861f
Benjamin Eidelman <beneidel@gmail.com>
2013-05-01 20:09:56 -0300

    run and copy commands

--- content/NugetTool.ps1
+++ content/NugetTool.ps1
@@ -1,4 +1,7 @@
 param(
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('Run','Copy')]
+    [string]$Command = "Run", # run or copy
     [string]$Package = "",
     [string]$Tool = "" # Using "" or "_" will find an exe or ps1 named as the package
 )
@@ -25,6 +28,25 @@ Function FindToolInFolder([string]$folder, [string]$packageName, [string]$tool) 
     }
 }
 
+Function FindPackageFolder([string]$packageName) {
+    $container = Resolve-Path "."
+    
+    # find in packages folder
+    while (!(Test-Path(Join-Path $container "packages"))) {
+        $containerParent = Join-Path $container ".." -resolve
+        if ($container -eq $containerParent) {
+            Throw "packages folder not found"
+        }
+        $container = $containerParent
+    }
+    $container = Join-Path $container "packages"
+    $packagePath = (Get-ChildItem -Path $container -Filter "$packageName.*" | Select-Object -First 1).FullName
+    if (!$packagePath) {
+        Throw "Package not found: $packageName"
+    }
+    return Join-Path $packagePath "tools"
+}
+
 Function FindPackageTool([string]$packageName, [string]$tool) {
     $container = Resolve-Path "."
     
@@ -41,21 +63,13 @@ Function FindPackageTool([string]$packageName, [string]$tool) {
     if ($toolPath) {
         return $toolPath
     }
+    $toolPath = FindToolInFolder (Join-Path $container $packageName\tools -ErrorAction SilentlyContinue) $packageName $tool
+    if ($toolPath) {
+        return $toolPath
+    }
     
     # find in packages folder
-    while (!(Test-Path(Join-Path $container "packages"))) {
-        $containerParent = Join-Path $container ".." -resolve
-        if ($container -eq $containerParent) {
-            Throw "packages folder not found"
-        }
-        $container = $containerParent
-    }
-    $container = Join-Path $container "packages"
-    $packagePath = (Get-ChildItem -Path $container -Filter "$packageName.*" | Select-Object -First 1).FullName
-    if (!$packagePath) {
-        Throw "Package not found: $packageName"
-    }
-    $packageToolsPath = Join-Path $packagePath "tools"
+    $packageToolsPath = FindPackageFolder $packageName
     $toolPath = FindToolInFolder $packageToolsPath $packageName $tool
     if ($toolPath) {
         return $toolPath
@@ -70,9 +84,30 @@ if ($Tool -eq "_") {
     $Tool = ""
 }
 
-$arguments = $args | %{ @($_) -join ", " }
 
-$command = (FindPackageTool $Package $Tool) + " " + $arguments
-"$command"
+if ($Command.ToLowerInvariant() -eq "copy") {
 
-Invoke-Expression $command
+    # Copy package tools folder
+    $CopyTo = "."
+    if (@($args).Length -gt 0) {
+        $CopyTo = @($args)[0]
+    }
+    $packageToolsPath = FindPackageFolder $Package
+    $targetPackageFolder = Join-Path $CopyTo $Package
+    if (Test-Path -PathType Container $targetPackageFolder) {
+        Remove-Item $targetPackageFolder -Force -Recurse
+    }
+    New-Item -path $targetPackageFolder -type directory -Force | Out-Null
+    Copy-Item $packageToolsPath $targetPackageFolder -recurse | Out-Null
+    "Copied $Package tools to $targetPackageFolder\tools"
+} elseif ($Command.ToLowerInvariant() -eq "run") {
+
+    # Run Tool
+    $arguments = $args | %{ @($_) -join ", " }
+    $expression = (FindPackageTool $Package $Tool) + " " + $arguments
+    "$expression"
+    Invoke-Expression $expression
+
+} else {
+    Throw "Command $Command not found."
+}
